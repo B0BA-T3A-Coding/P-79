6fb2c6c1c1f97fb645cfb68a2a5a43cfc5b21ae5
TT <83678908+B0BA-T3A-Coding@users.noreply.github.com>
2021-09-24 20:55:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-menu_list_array = ["Veg Margherita Pizza", "Chicken Tandoori Pizza", 
-"Veggie Pizza", "Paneer Tikka Pizza", "Pepperoni Pizza", 
-"Cheese Pizza", "Hawaiian pizza" ];
+menu_list_array =["Cheese Pizza","Pepperoni Pizza","Veggie Pizza","Hawaiian Pizza"];
 
 function getmenu(){
 var htmldata;
 menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        for(var i=0; i < menu_list_array.length; i++){
+            htmldata=htmldata+ menu_list_array[i] + "<br>";
         }
         document.getElementById("display_menu").innerHTML = htmldata;
-    
-         
+        //give the appropriate id name as display_menu  
 }
 
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
-menu_list_array.sort();
-        htmldata=""
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+ menu_list_array.sort();
+        // use the sort function as - menu_list_array.sort();
+        htmldata=" ";
+        for(var i=0; i < menu_list_array.length; i++){
+            htmldata =htmldata+menu_list_array[i]+"<br>";
         }
-         document.getElementById("display_newmenu").innerHTML = htmldata;		
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
 
 function add_top(){
